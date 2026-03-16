6d67d428e91c92a6cf2247684db6c1c54a240efc
junlinp <junlinp@deepmirror.com>
2026-03-16 17:04:57 +0800

    Update convert_onnx script

--- convert_onnx.sh
+++ convert_onnx.sh
@@ -1,23 +1,3 @@
-# 480 / 2 -> 240. closest 32 div is 256
-# 848 / 2 -> 424 closest  32 div is 448
-python scripts/make_onnx.py \
-  --model_dir weights/20-30-48/model_best_bp2_serialize.pth \
-  --save_path output/256x448 \
-  --height 256 \
-  --width 448 \
-  --valid_iters 16 \
-  --max_disp 192
-
-# 640 / 2 -> 320
-# 544 / 2 -> 272 need to 288
-python scripts/make_onnx.py \
-  --model_dir weights/20-30-48/model_best_bp2_serialize.pth \
-  --save_path output/320x288 \
-  --height 320 \
-  --width 288 \
-  --valid_iters 16 \
-  --max_disp 192
-
 # 480x864 full-resolution, low_memory=1 to reduce intermediate tensor sizes
 python scripts/make_onnx.py \
   --model_dir weights/20-30-48/model_best_bp2_serialize.pth \
