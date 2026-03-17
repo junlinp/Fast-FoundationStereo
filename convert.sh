cb113f3dfdf78bbf1659f3678a775c4065374305
junlinp <junlinp@deepmirror.com>
2026-03-17 17:14:01 +0800

    disable the imshow in run_demo_tensorrt

--- convert.sh
+++ convert.sh
@@ -27,3 +27,17 @@ python scripts/make_onnx.py \
   --width 288 \
   --valid_iters 16 \
   --max_disp 192
+
+#trtexec --onnx=output/480x864/feature_runner.onnx --saveEngine=output/480x864/feature_runner.engine --fp16  --useCudaGraph
+#trtexec --onnx=output/480x864/post_runner.onnx --saveEngine=output/480x864/post_runner.engine --fp16  --useCudaGraph
+
+python scripts/run_demo_tensorrt.py \
+  --onnx_dir output/480x864/ \
+  --left_file test_data/left.png \
+  --right_file test_data/right.png \
+  --intrinsic_file test_data/K.txt \
+  --out_dir output/trt_inference_480x864/ \
+  --remove_invisible 0 \
+  --denoise_cloud 0 \
+  --get_pc 0 \
+  --zfar 100 
